74902a19587500be874319096f656d9ed2af1cfe
Curt J. Sampson <cjs@cynic.net>
2024-01-23 12:26:04 +0900

    README: Indicate this is a fork repo; remove all other files

---
+++ README.md
@@ -0,0 +1,7 @@
+Fork Repo
+=========
+
+This is a fork of [virtualenv] with only development branches.
+If you're looking for the real repo, see that upstream.
+
+[virtualenv]: https://github.com/pypa/virtualenv.git
--- tests/unit/config/test___main__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import, unicode_literals
-
-import subprocess
-import sys
-
-
-def test_main():
-    out = subprocess.check_output([sys.executable, "-m", "virtualenv", "--help"], universal_newlines=True)
-    assert out
